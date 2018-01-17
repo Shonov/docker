5832cf26761f66357afe32bd64f143628e9759b8
vitaly shonov <v-shonov@mail.ru>
2018-01-17 12:21:41 +0300

    bugfix

--- tmp/docker/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connect = new mysqli('mysql', 'root', 'test', 'test');
-
-if ($connect->connect_error) die('Ошибка подключения ('. $connect->connect_errno . ')' . $connect->connect_error);
-
-$result = $connect->query("SELECT * FROM pet");
-
-var_dump($result);
